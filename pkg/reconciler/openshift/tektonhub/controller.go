f61810c81f2ef7ee466542a9944c567aa03a04b8
vinamra28 <vinjain@redhat.com>
2021-10-14 10:59:53 +0530

    modified

--- pkg/reconciler/openshift/tektonhub/controller.go
+++ pkg/reconciler/openshift/tektonhub/controller.go
@@ -2,8 +2,9 @@ package tektonhub
 
 import (
 	"context"
+	"fmt"
 
-	k8s_ctrl "github.com/tektoncd/operator/pkg/reconciler/kubernetes/tektonpipeline"
+	k8s_ctrl "github.com/tektoncd/operator/pkg/reconciler/kubernetes/tektonhub"
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
 )
@@ -11,5 +12,6 @@ import (
 // NewController initializes the controller and is called by the generated code
 // Registers eventhandlers to enqueue events
 func NewController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	fmt.Println("Openshift Controller entry")
 	return k8s_ctrl.NewExtendedController(OpenShiftExtension)(ctx, cmw)
 }
